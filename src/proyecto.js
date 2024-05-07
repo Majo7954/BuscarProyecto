5997dbd4cb5e01cacc4ccfecf8b4c5caf3de9621
Maria Jose Murillo <murillomendoza.mariajose@gmail.com>
2024-05-07 10:22:20 -0400

    no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos

---
+++ src/proyecto.js
@@ -0,0 +1,5 @@
+function buscarProyecto(nombre, proyectos){
+    return "";
+}
+
+export default buscarProyecto;
---
+++ src/proyecto.spec.js
@@ -0,0 +1,22 @@
+import buscarProyecto from "./proyecto.js";
+
+//Casos
+//Buscar el proyecto en una lista vacia
+//Buscar y encontrar un proyecto en una lista de 1 proyecto
+//Buscar y encontrar un proyecto en una lista de varios proyectos
+//Buscar y encontrar mas de una coindencia de proyectos
+//Buscar y encontrar proyectos cuyo nombre empieza con el criterio de busqueda
+         // por ejm: "ejerc" y en mi lista tengo "ejercio1", "ejercicio2" -> devuelve ambos
+
+describe("Buscar", () => {
+
+  it("no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => {
+    let proyectos = [];
+    expect (buscarProyecto("ejercicio1", proyectos)).toEqual("");
+  });
+  
+});
+
+
+
+  
